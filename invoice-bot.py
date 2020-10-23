e51def80be4cde5d5bfaab5c4fd53c489d9e6c48
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-10-23 11:12:09 -0500

    feat: Added kick/ban/unban features.
    Also added the ability to load cogs. This feels similar to how React uses components to separate commands and pieces into discrete packages that can be used more surgically than just lumping everything into one huge file.

--- invoice-bot.py
+++ invoice-bot.py
@@ -1,12 +1,25 @@
+#  https://www.youtube.c om/watch?v=THj99FuPJmI&t=20s
+
 # Imports - we just want to pull in the module and extract commands.
 import discord
 import random
+import os
 from config import api_key
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
 
 # When the bot has the information it needs from Discord, the bot is in a ready state. It is ready to execute its function. Read docs for the on_ready state.
+
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
 
 @client.event
@@ -51,14 +64,45 @@ async def _8ball(ctx, *, question):
         "Don't count on it",
         "My reply is no",
     ]
-
     answer = random.choice(responses)
 
     await ctx.send(f"Question: {question} \nAnswer: {answer}")
+
+
+# Grabbing a member as a discord.Member object allows us to access information about that user. This might be useful later on when I need to track which users have submitted invoices and which users haven't.
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+            return
 
 
 @client.command()
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
     # .run() will take the token as an identifying argument.
-client.run("NzE3MTY3Mjc5MjU0Nzk4MzY2.XtWYEw.7eN5RN8M8eGGnD8OvedItFesq-M")
+
+
+for filename in os.listdir('./cogs')
+if filename.endswith('.py'):
+    client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(api_key)
